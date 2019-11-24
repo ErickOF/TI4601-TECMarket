d0792d83f2401eb4978584a00f03cf50038528f0
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-11-24 02:25:07 -0600

    Routes were modified

--- Presentation/src/app/components/sidebar/sidebar.component.ts
+++ Presentation/src/app/components/sidebar/sidebar.component.ts
@@ -13,7 +13,7 @@ export const ROUTES: RouteInfo[] = [
   { path: '/dashboard', title: 'Dashboard', icon: 'ni-tv-2 text-primary', class: '' },
   { path: '/icons', title: 'Icons', icon: 'ni-planet text-blue', class: '' },
   { path: '/maps', title: 'Maps', icon: 'ni-pin-3 text-orange', class: '' },
-  { path: '/user-profile', title: 'User profile', icon: 'ni-single-02 text-yellow', class: '' },
+  // { path: '/user-profile', title: 'User profile', icon: 'ni-single-02 text-yellow', class: '' },
   { path: '/tables', title: 'Tables', icon: 'ni-bullet-list-67 text-red', class: '' },
   { path: '/login', title: 'Login', icon: 'ni-key-25 text-info', class: '' },
   { path: '/register', title: 'Register', icon: 'ni-circle-08 text-pink', class: '' }
@@ -23,7 +23,7 @@ export const CLIENT: RouteInfo[] = [
   { path: '/icons', title: 'Buy', icon: 'ni-cart text-blue', class: '' },
   { path: '/icons', title: 'Purchase History', icon: 'ni-bullet-list-67 text-orange', class: '' },
   { path: '/icons', title: 'Supermarkets', icon: 'ni-shop text-blue', class: '' },
-  { path: '/user-profile', title: 'User profile', icon: 'ni-single-02 text-yellow', class: '' }
+  { path: '/client/user-profile', title: 'User profile', icon: 'ni-single-02 text-yellow', class: '' }
 ];
 
 export const EMPLOYEE: RouteInfo[] = [
--- Presentation/src/app/layouts/client-layout/client-layout.module.ts
+++ Presentation/src/app/layouts/client-layout/client-layout.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { ClientLayoutRoutes } from './client-layout.routing';
+
+import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
 
 
 @NgModule({
@@ -13,6 +16,7 @@ import { ClientLayoutRoutes } from './client-layout.routing';
     ReactiveFormsModule
   ],
   declarations: [
+    UserProfileComponent
   ]
 })
 export class ClientLayoutModule { }
